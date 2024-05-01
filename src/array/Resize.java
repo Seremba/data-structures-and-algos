d7fbc818aee98d6f89ebf9197fbbfff275ca0f0c
Seremba <pserembae@gmail.com>
2024-05-01 17:47:37 +0300

    how to resize an array

--- src/array/Resize.java
+++ src/array/Resize.java
@@ -1,2 +1,20 @@
-package array;public class Resize {
+package array;
+public class Resize {
+	public static int[] resize(int[] arr, int capacity) {
+		int[] temp = new int[capacity];
+		for (int i = 0; i < arr.length; i++) {
+			temp[i] = arr[i];
+		}
+		arr = temp;
+		return arr;
+	}
+	
+	public static void main(String[] args) {
+		int[] original = new int[] {2, 4, 5, 9, 10};
+		System.out.println("Size of origial array " + original.length);
+		
+		original = resize(original, 10);
+		
+		System.out.println("Size of original array after resizing, " + original.length);
+	}
 }

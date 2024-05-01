a9cf62b2ab320feef4a3746993e05029ff06ee18
Seremba <pserembae@gmail.com>
2024-05-01 17:31:42 +0300

    Revert "how to find the number of nodes values of singly linked list"
    This reverts commit 27fa87cf15f4eb1ac14114c72ad9d71b2685b17c.

--- src/array/IsPalindrome.java
+++ src/array/IsPalindrome.java
@@ -1,25 +1,2 @@
-package array;
-
-public class IsPalindrome {
-	// Given a string, the task is to check whether it is palindrome o not
-	// example: madam -> true, that -> false
-	public static void main(String[] args) {
-		boolean ans = isPalindrome("that");
-		System.out.println(ans);
-	}
-	
-	public static boolean isPalindrome(String word) {
-		char [] charArray = word.toCharArray();
-		int start = 0;
-		int end = charArray.length - 1;
-		
-		for (int i = 0; i < charArray.length; i++) {
-			if (charArray[start] != charArray[end]) {
-				return false;
-			}
-			start++;
-			end--;
-		}
-		return true;
-	}
+package array;public class IsPalindrome {
 }

1a4a72c84a2e89d527d1d3466f7f22e23abcdafb
Seremba <pserembae@gmail.com>
2024-05-08 11:09:20 +0300

    remove a given key from a singly linked list

--- src/linkedlist/SinglyLinkedList.java
+++ src/linkedlist/SinglyLinkedList.java
@@ -208,7 +208,25 @@ public class SinglyLinkedList {
 		temp.next = newNode;
 		return head;
 	}
-	
+	// remove a given key from a singly linked list
+	public void deleteNode(int key) {
+		ListNode current = head;
+		ListNode temp = null;
+		
+		if (current != null && current.data == key) {
+			head = current.next;
+			return;
+		}
+		while (current != null && current.data != key) {
+			temp = current;
+			current = current.next;
+		}
+		if (current == null) {
+			return;
+		}
+		
+		temp.next = current.next;
+	}
 	public static void main(String[] args) {
 		SinglyLinkedList ssl = new SinglyLinkedList();
 		ssl.insertFirst(16);
@@ -216,10 +234,9 @@ public class SinglyLinkedList {
 		ssl.insertFirst(8);
 		ssl.insertFirst(1);
 		
-		
-		ssl.display(ssl.head);
 		ssl.insertInSortedList(11);
 		ssl.display(ssl.head);
-	
+		ssl.deleteNode(10);
+		ssl.display(ssl.head);
 	}
 }

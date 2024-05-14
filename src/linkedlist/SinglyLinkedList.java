20eea10b6f19ee5a5ed70d2c781023734e091ba2
Seremba <pserembae@gmail.com>
2024-05-15 01:52:46 +0300

    add two singly linked list: leetcode challenge

--- src/linkedlist/SinglyLinkedList.java
+++ src/linkedlist/SinglyLinkedList.java
@@ -337,6 +337,28 @@ public class SinglyLinkedList {
 		return dummy.next;
 	}
 	
+	// add two singly linked list https://leetcode.com/problems/add-two-numbers/description/
+	public static ListNode add(ListNode a, ListNode b) {
+		ListNode dummy = new ListNode(0);
+		ListNode tail = dummy;
+		int carry = 0;
+		
+		while (a != null || b != null) {
+			int x = (a != null) ? a.data : 0;
+			int y = (b != null) ? b.data : 0;
+			int sum = carry + x + y;
+			carry = sum / 10;
+			tail.next = new ListNode(sum % 10);
+			tail = tail.next;
+			if (a != null) a = a.next;
+			if (b != null) b = b.next;
+		}
+		if (carry > 0) {
+			tail.next = new ListNode(carry);
+		}
+		return dummy.next;
+	}
+	
 	public static void main(String[] args) {
 		SinglyLinkedList ssl1 = new SinglyLinkedList();
 		ssl1.insertLast(1);
